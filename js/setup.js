34efee6e7547bd8c5097a1c97e40855db65cf837
Spin <alexeioparin@gmail.com>
2020-06-04 15:01:50 +0300

    Настройка персонажей

--- js/setup.js
+++ js/setup.js
@@ -5,11 +5,12 @@ var USERS_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'В�
 var COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 var wizards = [];
+
 for (var i = 0; i < 4; i++) {
   var wizard = {};
-  var getRandom = function(mass) {
-    return Math.floor(Math.random() * mass.length)
-  }
+  var getRandom = function (mass) {
+    return Math.floor(Math.random() * mass.length);
+  };
   var b = getRandom(USERS_NAME);
   var c = getRandom(USERS_SURNAME);
   wizard.name = USERS_NAME[b] + ' ' + USERS_SURNAME[c];
@@ -22,17 +23,30 @@ for (var i = 0; i < 4; i++) {
   wizard.eyesColor = EYES[b];
   EYES.splice(b, 1);
   wizards[i] = wizard;
-};
+}
+
 var setupBlock = document.querySelector('.setup');
 setupBlock.classList.remove('hidden');
+
+
 var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var wizardList = document.querySelector('.setup-similar-list');
-for (var i = 0; i < wizards.length; i++) {
+var getWizardElement = function (obj) {
   var wizardElement = template.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-  wizardElement.querySelector('.wizard-coat').setAttribute('style', 'fill: ' + wizards[i].coatColor);
-  wizardElement.querySelector('.wizard-eyes').setAttribute('style', 'fill: ' + wizards[i].eyesColor);
-  wizardList.appendChild(wizardElement);
-}
+  wizardElement.querySelector('.setup-similar-label').textContent = obj.name;
+  wizardElement.querySelector('.wizard-coat').setAttribute('style', 'fill: ' + obj.coatColor);
+  wizardElement.querySelector('.wizard-eyes').setAttribute('style', 'fill: ' + obj.eyesColor);
+  return wizardElement;
+};
+
+var setWizardElement = function (mass) {
+  var fragment = document.createDocumentFragment();
+  for (i = 0; i < mass.length; i++) {
+    fragment.appendChild(getWizardElement(mass[i]));
+  }
+  return fragment;
+};
+
+wizardList.appendChild(setWizardElement(wizards));
 
 document.querySelector('.setup-similar').classList.remove('hidden');
