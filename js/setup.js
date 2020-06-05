08b876942c766eb1506b8ff5649cfcf75852c7d2
Spin <alexeioparin@gmail.com>
2020-06-05 14:35:53 +0300

    Настройка персонажей

--- js/setup.js
+++ js/setup.js
@@ -1,24 +1,29 @@
 'use strict';
 
-var USERS_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var USERS_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var USERS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var USERS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_NUMBER = 4;
 var wizards = [];
+var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-for (var i = 0; i < 4; i++) {
+var getRandom = function (mass) {
+  return Math.floor(Math.random() * mass.length);
+};
+var b = getRandom(USERS_NAMES);
+var c = getRandom(USERS_SURNAMES);
+
+/* Формирование списка свойств магов */
+
+for (var i = 0; i < WIZARD_NUMBER; i++) {
   var wizard = {};
-  var getRandom = function (mass) {
-    return Math.floor(Math.random() * mass.length);
-  };
-  var b = getRandom(USERS_NAME);
-  var c = getRandom(USERS_SURNAME);
-  wizard.name = USERS_NAME[b] + ' ' + USERS_SURNAME[c];
-  USERS_NAME.splice(b, 1);
-  USERS_SURNAME.splice(c, 1);
-  b = getRandom(COLOR);
-  wizard.coatColor = COLOR[b];
-  COLOR.splice(b, 1);
+  wizard.name = USERS_NAMES[b] + ' ' + USERS_SURNAMES[c];
+  USERS_NAMES.splice(b, 1);
+  USERS_SURNAMES.splice(c, 1);
+  b = getRandom(COLORS);
+  wizard.coatColor = COLORS[b];
+  COLORS.splice(b, 1);
   b = getRandom(EYES);
   wizard.eyesColor = EYES[b];
   EYES.splice(b, 1);
@@ -28,21 +33,23 @@ for (var i = 0; i < 4; i++) {
 var setupBlock = document.querySelector('.setup');
 setupBlock.classList.remove('hidden');
 
+/* Создание DOM элемента из объекта */
 
-var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var wizardList = document.querySelector('.setup-similar-list');
-var getWizardElement = function (obj) {
+var getWizardElement = function (userDescription) {
   var wizardElement = template.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = obj.name;
-  wizardElement.querySelector('.wizard-coat').setAttribute('style', 'fill: ' + obj.coatColor);
-  wizardElement.querySelector('.wizard-eyes').setAttribute('style', 'fill: ' + obj.eyesColor);
+  wizardElement.querySelector('.setup-similar-label').textContent = userDescription.name;
+  wizardElement.querySelector('.wizard-coat').setAttribute('style', 'fill: ' + userDescription.coatColor);
+  wizardElement.querySelector('.wizard-eyes').setAttribute('style', 'fill: ' + userDescription.eyesColor);
   return wizardElement;
 };
 
-var setWizardElement = function (mass) {
+/* Отрисовка DOM элемента */
+
+var setWizardElement = function (allWizards) {
   var fragment = document.createDocumentFragment();
-  for (i = 0; i < mass.length; i++) {
-    fragment.appendChild(getWizardElement(mass[i]));
+  for (i = 0; i < allWizards.length; i++) {
+    fragment.appendChild(getWizardElement(allWizards[i]));
   }
   return fragment;
 };
